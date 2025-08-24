ab25b8ca39dee91860b11e9417a00c0bd1729173
Sebastian Martinez <sebamar88@gmail.com>
2025-08-23 21:01:55 -0300

    feat: Add index files for translation and language detection exports

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export { translate } from "./translate";
+export { detectLanguage } from "./detectLanguage";
+export type { TranslateOptions, ClientConfig } from "./types";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.detectLanguage = exports.translate = void 0;
+var translate_1 = require("./translate");
+Object.defineProperty(exports, "translate", { enumerable: true, get: function () { return translate_1.translate; } });
+var detectLanguage_1 = require("./detectLanguage");
+Object.defineProperty(exports, "detectLanguage", { enumerable: true, get: function () { return detectLanguage_1.detectLanguage; } });
+//# sourceMappingURL=index.js.map
